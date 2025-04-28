d302525d8b54cfd914d799d19108e59b3ce40e8d
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-04-28 09:37:09 -0400

    Class Binding

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 <counter></counter>
 <TemplateSyntax />
 <ComputedComponent />
+<ClassComponent class="foo bar"/>
+<div style="margin-top: 15rem"></div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue'
 import Counter from './components/Counter.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue';
 import ComputedComponent from './components/ComputedComponent.vue';
+import ClassComponent from './components/ClassComponent.vue';
 
 const app = createApp(App);
 app.component('Counter', Counter);
 app.component('TemplateSyntax', TemplateSyntax);
 app.component('ComputedComponent', ComputedComponent);
+app.component('ClassComponent', ClassComponent);
 app.mount('#app');
 
