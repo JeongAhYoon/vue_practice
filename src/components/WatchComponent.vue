eb018ef890eebe3c808fb690366b995fc7f2b885
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-04-28 21:11:55 -0400

    watch with ref value

--- src/components/WatchComponent.vue
+++ src/components/WatchComponent.vue
@@ -1,31 +1,53 @@
 <template>
-    <h2>Watch Component</h2>
-    <div>
-        <p>Ask a Yes/No Question</p>
-        <input type="text" v-model="question" />
-        <div>Answer: {{answer}}</div>
-        <img :src="responseData.image" style="width: 200px" />
-    </div>
+  <h2>Watch Component</h2>
+  <div>
+    <p>Ask a Yes/No Question</p>
+    <input type="text" v-model="question" />
+    <div>Answer: {{ answer }}</div>
+    <img :src="responseData.image" style="width: 200px" />
+  </div>
+
+  <div>x: <input type="text" v-model="x" /></div>
+  <div>y: <input type="text" v-model="y" /></div>
 </template>
 
 <script setup>
-import { ref,watch } from 'vue';
-const question = ref('');
-const answer = ref('Question generally contains ?')
-const responseData = ref('');
+import { ref, watch } from "vue";
+const question = ref("");
+const answer = ref("Question generally contains ?");
+const responseData = ref("");
 watch(question, async (newQuesion, oldQuestion) => {
-    if(newQuesion.indexOf('?') > -1) {
-        answer.value = 'Thinking.....';
-        responseData.value ='';
-        try{
-            const res = await fetch('https://yesno.wtf/api');
-            const resJson = await res.json();
-            responseData.value = resJson;
-            answer.value = resJson.answer;
-        } catch(err) {
-                answer.value = 'Error!. Could not reach the API'
-        }
+  if (newQuesion.indexOf("?") > -1) {
+    answer.value = "Thinking.....";
+    responseData.value = "";
+    try {
+      const res = await fetch("https://yesno.wtf/api");
+      const resJson = await res.json();
+      responseData.value = resJson;
+      answer.value = resJson.answer;
+    } catch (err) {
+      answer.value = "Error!. Could not reach the API";
     }
-
+  }
 });
+const x = ref(0);
+const y = ref(0);
+// watch(
+//   () => x.value + y.value,
+//   (sum) => {
+//     console.log(`sum is ${sum}`);
+//   }
+// );
+watch([x, () => y.value], ([newX, newY]) => {
+  console.log(`value of x, y is ${newX} and ${newY}`);
+});
+// watch(x, (newX) => {
+//     console.log(`value of x is ${newX}`);
+// })
+// watch(y, (newY) => {
+//     console.log(`value of y is ${newY}`);
+// });
+// watch([x,y], ([newX,newY]) => {
+//     console.log(`value of x, y is ${newX} and ${newY}`);
+// })
 </script>
