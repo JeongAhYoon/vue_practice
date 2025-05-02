1a4feb6c97a4c5689efbbb9d8de4d347625e1be2
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-05-02 19:19:25 -0400

    Reacting to the Ref value State Changes

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,9 +1,8 @@
 <template>
-    
+    <div>Load Post Id: <button v-for="n in 5" :key="n" @click="postId = n">{{ n }}</button></div>
   <div v-if="error">Error Encountered: {{ error.message }}</div>
-  <div v-else-if="posts">
+  <div v-else-if="post">
     <NewSingleComponent
-      v-for="post in posts"
       :post="post"
       :key="post.id"
       @edit-post="updatePost"
@@ -13,17 +12,23 @@
 </template>
 
 <script setup>
-import { doFetch } from "@/useFetch";
+import { useFetch } from "@/useFetch";
 import NewSingleComponent from "./NewSingleComponent.vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 // const posts = ref([
 //     {id : 1, title: 'Post 1'},
 //     {id : 2, title: 'Post 2'},
 //     {id : 3, title: 'Post 3'},
 // ]);
 
-const {posts, error} = await doFetch();
-console.log(posts.value);
+const postId = ref('1');
+
+
+
+
+const url = computed(() => {return  `https://jsonplaceholder.typicode.com/posts/${postId.value}`});
+const {post, error} = useFetch(url);
+console.log(post.value);
 
 
 const updatePost = (id, value) => {
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,17 +1,36 @@
-import { ref } from "vue";
+import { isRef, ref, unref, watchEffect } from "vue";
 
-export async function doFetch() {
-    
-    const posts = ref(null);
-    const error = ref(null);
-    
-        try {
-          const data = await fetch("https://jsonplaceholder.typicode.com/posts");
-          posts.value = await data.json();
-          console.log(posts.value);
-        } catch (err) {
-          error.value = e;
-        }
-      
-    return {posts, error};
+export function useFetch(url) {
+  const post = ref(null);
+  const error = ref(null);
+  async function doFetch() {
+    post.value = null;
+    error.value = null;
+    const urlValue = unref(url);
+    try {
+      await timeout();
+      const res = await fetch(urlValue);
+      post.value = await res.json();
+    } catch (e) {
+      error.value = e;
+    }
+  }
+
+  if (isRef(url)) {
+    watchEffect(doFetch);
+  } else {
+    doFetch();
+  }
+
+  function timeout() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let num = Math.random();
+        console.log(num);
+        if (num < 0.3) reject(new Error("Randome error"));
+        else resolve();
+      }, 500);
+    });
+  }
+  return { post, error };
 }
