ae411ea078de62adb4261ba2dd444d9c2bd70ea8
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-05-03 09:03:43 -0400

    JavaScript Hooks for the Vue Transitions

--- src/components/TransitionComponent.vue
+++ src/components/TransitionComponent.vue
@@ -2,7 +2,15 @@
   <div>
     <button @click="show = !show">Toggle</button>
   </div>
-  <Transition enter-active-class="animate__animated animate__tada" leave-active-class="animate__animated animate__bounce">
+  <Transition
+    @before-enter="onBeforeEnter"
+    @enter="onEnter"
+    @after-enter="onAfterEnter"
+    @before-leave="onBeforeLeave"
+    @leave="onLeave"
+    @after-leave="onAfterLeave"
+
+  >
     <div v-if="show" style="text-align: center">Transition Example</div>
   </Transition>
 </template>
@@ -11,12 +19,36 @@
 import { ref, Transition } from "vue";
 
 const show = ref(true);
+
+const onBeforeEnter = (el) => {
+  console.log('on before enter', el)
+};
+const onEnter = (el, done) => {
+  const animate = el.animate([{transform: 'scale(0)'}, {}],{duration: 1000})
+  //done();
+  animate.onfinish = () => {
+    done();
+  }
+};
+const onAfterEnter = (el) => {};
+
+const onBeforeLeave = (el) => {};
+const onLeave = (el, done) => {
+  const animate = el.animate([{ transform: 'scale(1)', opacity: 1 },
+  { transform: 'scale(0)', opacity: 0 }],{ duration: 500, easing: 'ease-in' })
+  //done();
+  animate.onfinish = () => {
+    done();
+  }
+};
+const onAfterLeave = (el) => {};
+
+
 </script>
 
 <style scoped>
-
 .bounce-active {
-  animation: bounce-in 5s
+  animation: bounce-in 5s;
 }
 
 .leave-active {
