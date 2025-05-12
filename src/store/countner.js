b97544c8029ceec130bd68c42c2213988ac9a544
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-05-12 08:23:01 -0400

    define the Store using the composition setup functionality in CompositionAPI

--- src/store/countner.js
+++ src/store/countner.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
 // export const useCounterStore = defineStore('counter', {
 //     state() {
@@ -13,19 +14,36 @@ import { defineStore } from "pinia";
 //     }
 // })
 
-export const useCounterStore = defineStore('counterStore', {
-    state:() => {
-        return {
-            count:0
-        }
-    },
-    //this could be just like a computed property
-    getters: {
-        doubleCount: (state) => state.count *2
-    },
-    actions: {
-        increment() {
-            this.count++;
-        }
+// export const useCounterStore = defineStore('counterStore', {
+//     state:() => {
+//         return {
+//             count:0
+//         }
+//     },
+//     //this could be just like a computed property
+//     getters: {
+//         doubleCount: (state) => state.count *2
+//     },
+//     actions: {
+//         increment() {
+//             this.count++;
+//         }
+//     }
+// });
+
+export const useCounterStore = defineStore('counterStore', () => {
+    //app level elements could go there even router, watch.......
+    const count = ref(0);
+
+    const doubleCount = computed(() => 
+        count.value *2
+    );
+
+    function increment() {
+        count.value++;
     }
-});
+
+    return {
+        count, doubleCount, increment
+    }
+})
