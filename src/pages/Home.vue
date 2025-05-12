e5bef79c3bc41b513334524721de07b4434cbd20
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-05-12 08:44:39 -0400

    Ways to use state in Piana

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,25 @@
 <template>
 <div>Home Page</div>
-<div>Counter: {{ counter.count }}</div>
-<div>DoubleCount: {{ counter.doubleCount }}</div>
-<button @click="counter.increment()">Incremenet</button>
+<div>Counter: {{ count }}</div>
+<div>DoubleCount: {{ doubleCount }}</div>
+<button @click="increment()">Incremenet</button>
 </template>
 
 <script setup>
 import { useCounterStore } from '@/store/countner';
+import { storeToRefs } from 'pinia';
+import { computed, ref, watchEffect } from 'vue';
 
 const props = defineProps(['name']);
-const counter = useCounterStore();
+const counter = useCounterStore(); // store will be created when this is called
+
+//const { count, doubleCount } = storeToRefs(counter);
+const count = computed(() => counter.count);
+const doubleCount = computed(() => counter.doubleCount);
+
+const {increment} = counter;
+
+
 
 console.log('home', props);
 </script>
