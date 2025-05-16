6f29f0dbc5ee0bf747f5c57383f8c5e3fd5206b6
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2025-05-16 16:47:29 -0400

    setup functionality in Composition API

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,56 +1,78 @@
 <template>
-<div>Home Page</div>
-<div>Counter: {{ count }}</div>
-<div>DoubleCount: {{ doubleCount }}</div>
-<!-- <div>DoubleValue: {{ doubleValue }}</div> -->
- <div>Name: {{name}} </div>
-<button @click="add()">Incremenet</button>
-<button @click="$reset()">Reset</button>
+  <div>Home Page</div>
+  <div>Counter: {{ count }}</div>
+  <div>DoubleCount: {{ doubleCount }}</div>
+  <!-- <div>DoubleValue: {{ doubleValue }}</div> -->
+  <div>Name: {{ name }}</div>
+  <button @click="add()">Incremenet</button>
+  <button @click="counter.$reset()">Reset</button>
 </template>
 
-<script>
-import { useCounterStore } from '@/store/countner';
-import { mapActions, mapState, mapWritableState } from 'pinia';
+<!-- <script>
+import { useCounterStore } from "@/store/countner";
+import { mapActions, mapState, mapWritableState } from "pinia";
 
 //how to use option API for Piana
 
 // export default {
 //     computed: {
 //         ...mapState(useCounterStore, ['count', 'doubleCount'])
-//     }, 
+//     },
 //     methods: {
 //         ...mapActions(useCounterStore, ['increment'])
 //     }
 // };
 
-export default {
-    computed: {
-        // ...mapState(useCounterStore, {
-        //     count: 'count',
-        //     doubleCount: 'doubleCount',
-        //     doubleValue: state => state.count + 1
-        // })
-        ...mapWritableState(useCounterStore, ['count', 'doubleCount','name'])
-    }, 
-    mounted() {
-        this.$subscribe((mutation, state) => {
-            console.log(mutation),
-            console.log(state)
-        });
-    },
+// export default {
+//     computed: {
+//         // ...mapState(useCounterStore, {
+//         //     count: 'count',
+//         //     doubleCount: 'doubleCount',
+//         //     doubleValue: state => state.count + 1
+//         // })
+//         ...mapWritableState(useCounterStore, ['count', 'doubleCount','name'])
+//     },
+//     mounted() {
+//         this.$subscribe((mutation, state) => {
+//             console.log(mutation),
+//             console.log(state)
+//         });
+//     },
 
-    methods: {
-        ...mapActions(useCounterStore, ['increment', '$reset', '$patch', '$subscribe']), //subscribe: store에서 state가 바뀔떄마다 자동으로 실행되는 콜백을 등록하는 메서드
-        add() {
-            this.$patch({
-                count: this.count + 1,
-                name: Math.random() + 'asdf'
+//     methods: {
+//         ...mapActions(useCounterStore, ['increment', '$reset', '$patch', '$subscribe']), //subscribe: store에서 state가 바뀔떄마다 자동으로 실행되는 콜백을 등록하는 메서드
+//         add() {
+//             this.$patch({
+//                 count: this.count + 1,
+//                 name: Math.random() + 'asdf'
 
-            });// multple mutations in one mutation.. we use $patch
-            // this.count++;
-            // this.name = Math.random() + 'asdf';
-        }
-    }
+//             });// multple mutations in one mutation.. we use $patch
+//             // this.count++;
+//             // this.name = Math.random() + 'asdf';
+//         }
+//     }
+// };
+</script> -->
+
+<script setup>
+import { useCounterStore } from '@/store/countner';
+import { mapActions, mapState, mapWritableState, storeToRefs } from 'pinia';
+
+const counter = useCounterStore();
+const {count, doubleCount, name} = storeToRefs(counter);
+// composition API does not have $reset()
+//$subscrie
+counter.$subscribe((mutation, state) => {
+  console.log(mutation);
+  console.log(state);
+})
+
+
+function add() {
+
+        counter.count++;
+        counter.name = Math.random() + 'asdf';
 };
+
 
 </script>
--- src/store/countner.js
+++ src/store/countner.js
@@ -14,39 +14,46 @@ import { computed, ref } from "vue";
 //     }
 // })
 
-export const useCounterStore = defineStore('counterStore', {
-    state:() => {
-        return {
-            count:0,
-            name: "Kelly Yoon"
-        }
-    },
-    //this could be just like a computed property
-    getters: {
-        doubleCount: (state) => state.count *2
-    },
-    actions: {
-        increment() {
-            this.count++;
-        }
-    }
-});
-
-// export const useCounterStore = defineStore("counterStore", () => {
-//   //app level elements could go there even router, watch.......
-//   const count = ref(0);
-
-//   const doubleCount = computed(() => count.value * 2);
-
-//   function increment() {
-//     count.value++;
-//   }
-
-//   return {
-//     count,
-//     doubleCount,
-//     increment,
-//   };
+// export const useCounterStore = defineStore('counterStore', {
+//     state:() => {
+//         return {
+//             count:0,
+//             name: "Kelly Yoon"
+//         }
+//     },
+//     //this could be just like a computed property
+//     getters: {
+//         doubleCount: (state) => state.count *2
+//     },
+//     actions: {
+//         increment() {
+//             this.count++;
+//         }
+//     }
 // });
+
+export const useCounterStore = defineStore("counterStore", () => {
+  //app level elements could go there even router, watch.......
+  const count = ref(0);
+  const name = ref('Kelly is studying Vue');
+
+  const doubleCount = computed(() => count.value * 2);
+
+  function increment() {
+    count.value++;
+  }
+  function $reset() {
+    count.value = 0;
+    name.value = 'Kelly Yoon dev'
+  }
+
+  return {
+    count,
+    doubleCount,
+    increment,
+    name,
+    $reset
+  };
+});
 
 
